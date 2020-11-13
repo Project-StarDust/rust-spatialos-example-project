6320081e508c38e3f4e01a42cff8a4c147c81d08
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-11-13 19:06:39 +0900

    :sparkles: Updated client_vtable

--- workers/client_vtable/src/main.rs
+++ workers/client_vtable/src/main.rs
@@ -1,48 +1,110 @@
-use spatialos_sdk::sys_exports::worker::connection::{ConnectionFuture, ConnectionParameters};
-use spatialos_sdk::sys_exports::worker::constraint::ComponentConstraint;
-use spatialos_sdk::sys_exports::worker::constraint::Constraint;
 use spatialos_sdk::sys_exports::worker::{
     connection::NetworkConnectionType, log_message::LogMessage, LogLevel,
 };
 use spatialos_sdk::sys_exports::worker::{
     connection::NetworkSecurityType,
     op::{
-        AddComponentOp, AddEntityOp, CommandRequestOp, ComponentUpdateOp, DisconnectOp,
-        EntityQueryResponseOp, LogMessageOp, RemoveEntityOp, WorkerOp,
+        AddComponentOp, CommandRequestOp, ComponentUpdateOp, DisconnectOp, EntityQueryResponseOp,
+        LogMessageOp, WorkerOp,
     },
     EntityQuery, ResultType,
 };
+use spatialos_sdk::sys_exports::worker::{
+    connection::{ConnectionFuture, ConnectionParameters},
+    constraint::EntityIdConstraint,
+};
 use spatialos_sdk::Component;
+use spatialos_sdk::{sys_exports::worker::constraint::Constraint, Type};
 
 mod schema_types;
+use schema_types::ClientData;
+use schema_types::Coordinates;
 use schema_types::Position;
 
-pub fn on_disconnect(op: &DisconnectOp) {
-    println!("Disconnected: {:?}", op)
-}
+const WORKER_TYPE: &str = "client_vtable";
+
+#[macro_use]
+extern crate spatialos_macro;
 
 pub fn on_log_message(op: &LogMessageOp) {
-    println!("LogMessage: {:?}", op)
+    println!("log: {}", op.message)
+}
+
+pub fn on_disconnect(op: &DisconnectOp) {
+    println!("disconnected. reason: {}", op.reason)
 }
 
 pub fn on_entity_query_response(op: &EntityQueryResponseOp) {
-    println!("EntityQueryResponse: {:?}", op)
-}
-
-pub fn on_add_entity(op: &AddEntityOp) {
-    println!("AddEntity: {:?}", op)
-}
-
-pub fn on_remove_entity(op: &RemoveEntityOp) {
-    println!("RemoveEntity: {:?}", op)
+    println!(
+        "entity query result: {} entities. Status: {:?}.",
+        op.result_count, op.status_code
+    );
+    if !op.results.is_empty() {
+        for index in 0..op.result_count {
+            let entity = &op.results[index as usize];
+            print!(
+                "- entity {} with {} components: ",
+                entity.entity_id, entity.component_count
+            );
+            for index2 in 0..entity.component_count {
+                let component = &entity.components[index2 as usize];
+                if component.component_id == Position::ID {
+                    let position = unsafe {
+                        Box::from_raw(component.user_handle as *mut <Position as Component>::Data)
+                    };
+                    print!("{:?}, ", *position);
+                    Box::into_raw(position);
+                }
+                if component.component_id == ClientData::ID {
+                    let client_data = unsafe {
+                        Box::from_raw(component.user_handle as *mut <ClientData as Component>::Data)
+                    };
+                    print!("{:?}, ", *client_data);
+                    Box::into_raw(client_data);
+                }
+            }
+            println!("");
+        }
+    }
 }
 
 pub fn on_add_component(op: &AddComponentOp) {
-    println!("AddComponent: {:?}", op)
+    println!(
+        "received add component op (entity: {}, component: {})",
+        op.entity_id, op.data.component_id
+    );
+
+    if op.data.component_id == Position::ID {
+        let position =
+            unsafe { Box::from_raw(op.data.user_handle as *mut <Position as Component>::Data) };
+        println!(
+            "received improbable.Position initial data ({}, {}, {})",
+            position.coords.x, position.coords.y, position.coords.z
+        );
+        Box::into_raw(position);
+    }
 }
 
 pub fn on_component_update(op: &ComponentUpdateOp) {
-    println!("ComponentUpdate: {:?}", op)
+    println!(
+        "received component update op (entity: {}, component: {})",
+        op.entity_id, op.update.component_id
+    );
+
+    if op.update.component_id == Position::ID {
+        let position =
+            unsafe { Box::from_raw(op.update.user_handle as *mut <Position as Component>::Update) };
+        if !position.coords.is_null() {
+            let coords =
+                unsafe { Box::from_raw(position.coords as *mut <Coordinates as Type>::Update) };
+            println!(
+                "received improbable.Position update data ({}, {}, {})",
+                coords.x, coords.y, coords.z
+            );
+            Box::into_raw(coords);
+        }
+        Box::into_raw(position);
+    }
 }
 
 pub fn on_command_request(op: &CommandRequestOp) {
@@ -50,14 +112,37 @@ pub fn on_command_request(op: &CommandRequestOp) {
 }
 
 fn main() {
-    let mut vtables = vec![Position::get_vtable().into()];
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 4 {
+        eprintln!(
+            "Usage: {} <hostname> <port> <worker_id>",
+            args.get(0).expect("Can't have program name")
+        );
+        eprintln!("    <hostname>    - hostname of the receptionist to connect to.");
+        eprintln!("    <port>        - port to use.");
+        eprintln!("    <worker_id>   - name of the worker assigned by SpatialOS.");
+        std::process::exit(1);
+    }
+
+    let hostname = args.get(1).unwrap();
+    let port = args
+        .get(2)
+        .unwrap()
+        .parse::<u16>()
+        .expect("Can't parse port to u16");
+    let worker_id = args.get(3).unwrap();
+
+    let mut vtables = vec![
+        Position::get_vtable().into(),
+        ClientData::get_vtable().into(),
+    ];
     vtables.shrink_to_fit();
 
     let parameters = {
         let mut parameters = ConnectionParameters::default();
         parameters.network.connection_type = NetworkConnectionType::ModularKcp;
         parameters.network.modular_kcp.security_type = NetworkSecurityType::Insecure;
-        parameters.worker_type = "test_rust".to_string();
+        parameters.worker_type = WORKER_TYPE.to_string();
         parameters.network.tcp.multiplex_level = 4;
         parameters.default_component_vtable = std::ptr::null();
         parameters.component_vtable_count = vtables.len() as u32;
@@ -65,23 +150,22 @@ fn main() {
         parameters
     };
     let mut connection = {
-        let mut connection =
-            ConnectionFuture::connect_async("localhost", 7777, "test_rust14", parameters);
+        let mut connection = ConnectionFuture::connect_async(hostname, port, worker_id, parameters);
         connection.get(None).expect("Timed out")
     };
     connection.send_log_message(LogMessage::new(
         LogLevel::Warn,
-        "test_rust",
-        "Salut a tous !",
+        WORKER_TYPE,
+        "Connected successfully",
         None,
     ));
 
     let query = {
         let mut query = EntityQuery::default();
-        query.constraint = Constraint::Component(ComponentConstraint { component_id: 54 });
+        query.constraint = Constraint::EntityId(EntityIdConstraint { entity_id: 1 });
         query.result_type = ResultType::Snapshot;
-        query.snapshot_result_type_component_id_count = 1;
-        query.snapshot_result_type_component_ids = vec![54];
+        query.snapshot_result_type_component_id_count = 2;
+        query.snapshot_result_type_component_ids = vec![Position::ID, ClientData::ID];
         query
     };
     connection.send_entity_query_request(query, None);
@@ -95,8 +179,6 @@ fn main() {
                 WorkerOp::EntityQueryResponse(query_response) => {
                     on_entity_query_response(&query_response)
                 }
-                WorkerOp::AddEntity(add_entity) => on_add_entity(&add_entity),
-                WorkerOp::RemoveEntity(remove_entity) => on_remove_entity(&remove_entity),
                 WorkerOp::AddComponent(add_component) => on_add_component(&add_component),
                 WorkerOp::ComponentUpdate(component_update) => {
                     on_component_update(&component_update)
--- workers/client_vtable/src/schema_types.rs
+++ workers/client_vtable/src/schema_types.rs
@@ -1,114 +1,180 @@
 use spatialos_sdk::sys_exports::schema::ComponentData;
 use spatialos_sdk::sys_exports::schema::ComponentUpdate;
 use spatialos_sdk::sys_exports::worker::ComponentId;
-use spatialos_sdk::Component;
+use spatialos_sdk::{Component, Type};
 use std::os::raw::c_void;
+
+#[allow(dead_code)]
+#[derive(SpatialComponent)]
+#[id(1001)]
+pub struct ClientData {
+    #[field_id(1)]
+    input_state: f32,
+}
+
+pub struct Position;
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct Position {
-    pub coords: Coordinates,
+pub struct PositionData {
+    pub coords: CoordinatesData,
 }
 
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct PositionUpdate {
-    pub coords: *mut Coordinates,
+    pub coords: *mut CoordinatesUpdate,
 }
+
+pub struct Coordinates;
 
 #[repr(C)]
 #[derive(Debug, Clone)]
-pub struct Coordinates {
+pub struct CoordinatesData {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
 
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct CoordinatesUpdate {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Type for Coordinates {
+    type Data = CoordinatesData;
+
+    type Update = CoordinatesUpdate;
+
+    fn type_data_deserialize(
+        _: *mut c_void,
+        source: &mut spatialos_sdk::sys_exports::schema::Object,
+    ) -> Self::Data {
+        let x = source.get_double(1);
+        let y = source.get_double(2);
+        let z = source.get_double(3);
+        Self::Data { x, y, z }
+    }
+
+    fn type_data_serialize(
+        _: *mut c_void,
+        data: &mut Self::Data,
+        target: &mut spatialos_sdk::sys_exports::schema::Object,
+    ) {
+        target.add_double(1, data.x);
+        target.add_double(2, data.y);
+        target.add_double(3, data.z);
+    }
+
+    fn type_update_deserialize(
+        _: *mut c_void,
+        source: &mut spatialos_sdk::sys_exports::schema::Object,
+    ) -> Self::Update {
+        let x = source.get_double(1);
+        let y = source.get_double(2);
+        let z = source.get_double(3);
+        Self::Update { x, y, z }
+    }
+
+    fn type_update_serialize(
+        user_data: *mut c_void,
+        data: &mut Self::Update,
+        target: &mut spatialos_sdk::sys_exports::schema::Object,
+    ) {
+        target.add_double(1, data.x);
+        target.add_double(2, data.y);
+        target.add_double(3, data.z);
+    }
+
+    fn type_update_free(user_data: *mut c_void, data: Self::Update) {}
+
+    fn type_update_copy(user_data: *mut c_void, data: &Self::Update) -> Self::Update {
+        data.clone()
+    }
+}
+
 impl Component for Position {
     type Update = PositionUpdate;
+    type Data = PositionData;
 
     const ID: ComponentId = 54;
 
     fn component_data_deserialize(
         _: ComponentId,
-        _: *mut c_void,
+        user_data: *mut c_void,
         mut source: ComponentData,
-    ) -> Self {
+    ) -> Self::Data {
         let mut fields = source.get_fields();
-        let mut object = fields.get_object(1);
-        let x = object.get_double(1);
-        let y = object.get_double(2);
-        let z = object.get_double(3);
-        Position {
-            coords: Coordinates { x, y, z },
-        }
+        let coords = Coordinates::type_data_deserialize(user_data, &mut fields.get_object(1));
+        Self::Data { coords }
     }
     fn component_data_serialize(
         _: ComponentId,
-        _: *mut c_void,
-        data: &mut Position,
+        user_data: *mut c_void,
+        data: &mut Self::Data,
     ) -> ComponentData {
         let mut component_data = ComponentData::new();
         let mut fields = component_data.get_fields();
-        let mut coords = fields.add_object(1);
-        coords.add_double(1, data.coords.x);
-        coords.add_double(2, data.coords.y);
-        coords.add_double(3, data.coords.z);
+        Coordinates::type_data_serialize(user_data, &mut data.coords, &mut fields.add_object(1));
         component_data
     }
 
     fn component_update_deserialize(
         _: ComponentId,
-        _: *mut c_void,
+        user_data: *mut c_void,
         mut source: ComponentUpdate,
     ) -> Self::Update {
         let mut fields = source.get_fields();
-        PositionUpdate {
-            coords: if fields.get_object_count(1) == 1 {
-                let mut coords = fields.get_object(1);
-                let x = coords.get_double(1);
-                let y = coords.get_double(2);
-                let z = coords.get_double(3);
-                let boxed_coords = Box::new(Coordinates { x, y, z });
-                Box::into_raw(boxed_coords)
-            } else {
-                std::ptr::null_mut()
-            },
-        }
+        let coords = if fields.get_object_count(1) == 1 {
+            Box::into_raw(Box::new(Coordinates::type_update_deserialize(
+                user_data,
+                &mut fields.get_object(1),
+            )))
+        } else {
+            std::ptr::null_mut()
+        };
+        Self::Update { coords }
     }
 
     fn component_update_serialize(
         _: ComponentId,
-        _: *mut c_void,
+        user_data: *mut c_void,
         data: &mut Self::Update,
     ) -> ComponentUpdate {
         let mut new_update = ComponentUpdate::new();
         let mut fields = new_update.get_fields();
         if !data.coords.is_null() {
-            let coords = unsafe { Box::from_raw(data.coords) };
-            let mut object = fields.add_object(1);
-            object.add_double(1, coords.x);
-            object.add_double(2, coords.y);
-            object.add_double(3, coords.z);
+            let mut coords = unsafe { Box::from_raw(data.coords) };
+            Coordinates::type_update_serialize(user_data, &mut *coords, &mut fields.add_object(1));
             Box::into_raw(coords);
         }
         new_update
     }
 
-    fn component_update_free(_: ComponentId, _: *mut c_void, data: Self::Update) {
-        if !data.coords.is_null() {
-            unsafe {
-                Box::from_raw(data.coords);
-            }
-        }
-    }
-    fn component_update_copy(_: ComponentId, _: *mut c_void, data: &Self::Update) -> Self::Update {
-        let mut new_data = data.clone();
+    fn component_update_free(_: ComponentId, user_data: *mut c_void, data: Self::Update) {
         if !data.coords.is_null() {
             let coords = unsafe { Box::from_raw(data.coords) };
-            new_data.coords = Box::into_raw(coords.clone());
-            Box::into_raw(coords);
+            Coordinates::type_update_free(user_data, *coords);
         }
+    }
+
+    fn component_update_copy(
+        _: ComponentId,
+        user_data: *mut c_void,
+        data: &Self::Update,
+    ) -> Self::Update {
+        let mut new_data = data.clone();
+        new_data.coords = if !data.coords.is_null() {
+            let coords = unsafe { Box::from_raw(data.coords) };
+            let new_coords = Coordinates::type_update_copy(user_data, &*coords);
+            Box::into_raw(coords);
+            Box::into_raw(Box::new(new_coords))
+        } else {
+            std::ptr::null_mut()
+        };
         new_data
     }
 }
